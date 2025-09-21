7150ecc03eac06d259bd5d76ccc42fbeb293e02d
Tran Vo <ngoctranvl2006@gmail.com>
2025-09-21 03:21:06 -0400

    afdh

--- next.config.mjs
+++ next.config.mjs
@@ -3,4 +3,5 @@ const nextConfig = {};
 console.log("NEXT_PUBLIC_SUPABASE_URL =", process.env.NEXT_PUBLIC_SUPABASE_URL);
 console.log("NEXT_PUBLIC_SUPABASE_ANON_KEY =", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ? "loaded" : "missing");
 
+
 export default nextConfig;
